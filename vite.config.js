43a02a9b0ea5427d7d0ed99130838e05cb79a972
tahmasb <hggvff222@gmail.com>
2023-02-12 15:16:09 +0330

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate'
+  })],
   css: {
     modules: {
       localsConvention: 'camelCase'

6f3dcebd453095464a37152fb1b45a0c24e629d7
tahmasb <hggvff222@gmail.com>
2023-02-18 14:52:00 +0330

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,60 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      devOptions: { enabled: true },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'landingpage',
+        short_name: 'landingpage',
+        description: 'my landing project mojtaba)',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     modules: {
       localsConvention: 'camelCase'
     }
   }
-})
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { VitePWA } from 'vite-plugin-pwa'
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react()],
+//   css: {
+//     modules: {
+//       localsConvention: 'camelCase'
+//     }
+//   }
+// })
